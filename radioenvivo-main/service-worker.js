2c8faf9e502d71957c5723b4c44a937205fa6c08
Fernando Medina <vivalaradio80@gmail.com>
2025-05-23 11:08:30 -0300

    Agrego imagen de fondo a la PWA

---
+++ radioenvivo-main/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('viva-radio-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
